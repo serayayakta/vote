2035a818f04a1a6e1c7a708616e27121c58640fa
Seray Ayakta <serayayakta@sabanciuniv.edu>
2023-05-28 03:49:17 +0300

    applicant name added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,22 +5,33 @@ import Kilicdaroglu from "../assets/kilicdaroglu-profile.jpeg";
 export default function Profile(props) {
   const image = props.name === "RECEP TAYYİP ERDOĞAN" ? Erdogan : Kilicdaroglu;
   return (
-    <div
-      style={{
-        display: "block",
-        width: "160px",
-        height: "200px",
-        overflow: "hidden",
-        marginLeft: "10px",
-        marginRight: "10px",
-      }}
-    >
-      <img
-        src={image}
+    <>
+      <div
         style={{
-          width: "100%",
+          display: "block",
+          width: "160px",
+          height: "200px",
+          overflow: "hidden",
+          marginLeft: "10px",
+          marginRight: "10px",
         }}
-      />
-    </div>
+      >
+        <img
+          src={image}
+          style={{
+            width: "100%",
+          }}
+        />
+      </div>
+      <h4
+        style={{
+          marginLeft: "10px",
+          maxWidth: "160px",
+          textAlign: "center",
+        }}
+      >
+        {props.name}
+      </h4>
+    </>
   );
 }
